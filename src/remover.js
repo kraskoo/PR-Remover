bab43235e8144da18349577361e0d7d016103c54
kraskoo <kraskoo87@gmail.com>
2019-09-15 23:15:47 +0300

    Remove low opacity from the rest containers

--- src/remover.js
+++ src/remover.js
@@ -1,15 +1,24 @@
-var divs = Array.from(document.querySelectorAll('div'));
-var divToRemove = null;
-var highestZIndex = -1;
-for (var div of divs) {
-	var zIndex = Number(window.getComputedStyle(div, null).getPropertyValue('z-index'));
-	if (highestZIndex < zIndex) {
-		highestZIndex = zIndex; divToRemove = div;
+(function () {
+	var divs = Array.from(document.querySelectorAll('div'));
+	var divToRemove = null;
+	var highestZIndex = -1;
+	for (var div of divs) {
+		var opacity = Number(window.getComputedStyle(div, null).getPropertyValue('opacity'));		
+		var zIndex = Number(window.getComputedStyle(div, null).getPropertyValue('z-index'));
+		if (!isNaN(zIndex) && highestZIndex < zIndex) {
+			highestZIndex = zIndex;
+			divToRemove = div;
+			continue;
+		}
+		
+		if (!isNaN(opacity) && opacity < 1) {
+			div.style.opacity = '1';
+		}
 	}
-}
 
-if (divToRemove && divToRemove.parentElement) {
-	divToRemove.parentElement.removeChild(divToRemove);
-}
+	if (divToRemove && divToRemove.parentElement) {
+		divToRemove.parentElement.removeChild(divToRemove);
+	}
 
-document.body.style.overflowY = 'auto';
+	document.body.style.overflowY = 'auto';
+}());
