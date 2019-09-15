311583abe5d89cd6b216a149deaa149be95e4e27
kraskoo <kraskoo87@gmail.com>
2019-09-15 23:40:48 +0300

    Update to remove all Pop-Ups with clickonce

--- src/remover.js
+++ src/remover.js
@@ -2,22 +2,28 @@
 	var divs = Array.from(document.querySelectorAll('div'));
 	var divToRemove = null;
 	var highestZIndex = -1;
-	for (var div of divs) {
-		var opacity = Number(window.getComputedStyle(div, null).getPropertyValue('opacity'));		
-		var zIndex = Number(window.getComputedStyle(div, null).getPropertyValue('z-index'));
-		if (!isNaN(zIndex) && highestZIndex < zIndex) {
-			highestZIndex = zIndex;
-			divToRemove = div;
-			continue;
+	while (true) {
+		for (var div of divs) {
+			var computedDivStyle = window.getComputedStyle(div, null);
+			var zIndex = Number(computedDivStyle.getPropertyValue('z-index'));
+			if (!isNaN(zIndex) && highestZIndex < zIndex) {
+				highestZIndex = zIndex;
+				divToRemove = div;
+				continue;
+			}
+			
+			var opacity = Number(computedDivStyle.getPropertyValue('opacity'));
+			if (!isNaN(opacity) && opacity < 1) {
+				div.style.opacity = '1';
+			}
 		}
-		
-		if (!isNaN(opacity) && opacity < 1) {
-			div.style.opacity = '1';
-		}
-	}
 
-	if (divToRemove && divToRemove.parentElement) {
-		divToRemove.parentElement.removeChild(divToRemove);
+		if (divToRemove && divToRemove.parentElement) {
+			divs = divs.filter(x => x !== divToRemove);
+			divToRemove.parentElement.removeChild(divToRemove);
+			divToRemove = null;
+			highestZIndex = -1;
+		} else break;
 	}
 
 	document.body.style.overflowY = 'auto';
