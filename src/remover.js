a5d296a21981675bf020596ce5d812f1d84a10a6
Krasimir Stefanov <kraskoo87@gmail.com>
2020-01-10 23:29:17 +0200

    Add check for visible element with opacity in front of page

--- src/remover.js
+++ src/remover.js
@@ -3,15 +3,20 @@
     .map(x => ({ el: x, cs: window.getComputedStyle(x, null), zIndex: Number(window.getComputedStyle(x, null).getPropertyValue('z-index')) }))
     .sort((a, b) => b.zIndex - a.zIndex)
     .forEach(x => {
-      if (!isNaN(x.zIndex) && x.zIndex > 0) {
-        var el = x.el;
+      var opacity = x.cs.getPropertyValue('opacity');
+      var visibility = x.cs.getPropertyValue('visibility');
+      var el = x.el;
+      if (opacity && Number(opacity) === 1 && visibility === 'visible') {
+        if (el.parentElement) {
+          el.parentElement.removeChild(el);
+        }
+      } else if (!isNaN(x.zIndex) && x.zIndex > 0) {
         if (el.parentElement) {
           el.parentElement.removeChild(el);
         }
       } else {
-        var opacity = Number(x.cs.getPropertyValue('opacity'));
-        if (!isNaN(opacity) && opacity < 1) {
-          x.el.style.opacity = '1';
+        if (!isNaN(Number(opacity)) && Number(opacity) < 1) {
+          el.style.opacity = '1';
         }
       }
     });
--- src/removerFirst.js
+++ src/removerFirst.js
@@ -1,19 +1,25 @@
 (function () {
-  var divs = Array.from(document.querySelectorAll('*'))
+  var elements = Array.from(document.querySelectorAll('*'))
       .map(x => ({ el: x, cs: window.getComputedStyle(x, null), zIndex: Number(window.getComputedStyle(x, null).getPropertyValue('z-index')) }))
       .sort((a, b) => b.zIndex - a.zIndex);
   var hasRemoveFirst = false;
-  divs.forEach(x => {
-    if (!hasRemoveFirst && !isNaN(x.zIndex)) {
-      var el = x.el;
-      if (el && el.parentElement) {
+  elements.forEach(x => {
+    var opacity = x.cs.getPropertyValue('opacity');
+    var visibility = x.cs.getPropertyValue('visibility');
+    var el = x.el;
+    if (!hasRemoveFirst && opacity && Number(opacity) === 1 && visibility === 'visible') {
+      if (el.parentElement) {
+        el.parentElement.removeChild(el);
+        hasRemoveFirst = !hasRemoveFirst;
+      }
+    } else if (!hasRemoveFirst && !isNaN(x.zIndex) && x.zIndex > 0) {
+      if (el.parentElement) {
         el.parentElement.removeChild(el);
         hasRemoveFirst = !hasRemoveFirst;
       }
     } else {
-      var opacity = Number(x.cs.getPropertyValue('opacity'));
-      if (!isNaN(opacity) && opacity < 1) {
-        x.el.style.opacity = '1';
+      if (!isNaN(Number(opacity)) && Number(opacity) < 1) {
+        el.style.opacity = '1';
       }
     }
   });
