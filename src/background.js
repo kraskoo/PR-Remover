b2fecf5ee7a86b07637050cb5cc22d531f705f7d
Krasimir Stefanov <kraskoo87@gmail.com>
2020-01-11 02:08:37 +0200

    Brand new version

--- src/background.js
+++ src/background.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var catchFirst = false;
+var url = null;
+
 chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({ removePopUpsOneByOne: true }, () => { });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
@@ -11,6 +14,21 @@ chrome.runtime.onInstalled.addListener(function () {
     }]);
   });
 });
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, function (tab) {
+    url = tab.url;
+  });
+})
+
+chrome.tabs.onUpdated.addListener(function (tabId, _changeInfo, _tab) {
+  chrome.tabs.get(tabId, function (tab) {
+    if (!catchFirst) {
+      url = tab.url;
+      catchFirst = !catchFirst;
+    }
+  });
+});
+
 chrome.browserAction.onClicked.addListener(function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     for (let tab of tabs) {
@@ -23,9 +41,11 @@ function removePopUp(tab) {
   if (tab.url.startsWith('chrome://')) {
     return;
   }
-  
+
   chrome.storage.sync.get('removePopUpsOneByOne', function (items) {
-    var filePath = items.removePopUpsOneByOne ? './removerFirst.js' : './remover.js';
-    chrome.tabs.executeScript(tab.id, { file: filePath });
+    var filePath = './remover.js';
+    chrome.tabs.executeScript(tab.id, { file: filePath }, function() {
+      chrome.tabs.sendMessage(tab.id, url);
+    });
   });
 }
--- src/remover.js
+++ src/remover.js
@@ -1,34 +1,10 @@
-(function () {
-  Array.from(document.querySelectorAll('div'))
-    .map(x => ({ el: x, cs: window.getComputedStyle(x, null), zIndex: Number(window.getComputedStyle(x, null).getPropertyValue('z-index')) }))
-    .sort((a, b) => {
-      var bVisibility = b.cs.getPropertyValue('visibility');
-      var aVisibility = a.cs.getPropertyValue('visibility');
-      if (bVisibility === 'visible') return 1;
-      else if (aVisibility === 'visible') return -1;
-      return b.zIndex - a.zIndex;
-    })
-    .forEach(x => {
-      if (!x.cs) return;
-      var opacity = x.cs.getPropertyValue('opacity');
-      var hasOpacity = opacity && opacity !== '';
-      opacity = Number(opacity);
-      var visibility = x.cs.getPropertyValue('visibility');
-      var el = x.el;
-      if (hasOpacity && !isNaN(opacity) && opacity === 1 && visibility === 'visible') {
-        if (el.parentElement) {
-          el.parentElement.removeChild(el);
-        }
-      } else if (!isNaN(x.zIndex) && x.zIndex > 0) {
-        if (el.parentElement) {
-          el.parentElement.removeChild(el);
-        }
-      } else {
-        if (!isNaN(opacity) && opacity < 1) {
-          el.style.opacity = '1';
-        }
-      }
-    });
-  document.body.style.overflow = 'auto';
-  document.documentElement.style.overflow = 'auto';
-}());
+chrome.runtime.onMessage.addListener(function (message, _sender, _sendResponse) {
+  var parser = new DOMParser();
+  var req = new XMLHttpRequest();
+  req.addEventListener("load", function () {
+    var doc = parser.parseFromString(this.responseText, 'text/html');
+    document.body = doc.body;
+  });
+  req.open("GET", message);
+  req.send();
+});
